97675b812e77a676b08fb1e7106bf1425c544579
mohammad43268 <huzaifa2086@gmail.com>
2026-08-13 05:17:23 +0530

    feat: module1 — final build

--- day-36-55-full-stack-withsupabase/dev-dist/sw.js
+++ day-36-55-full-stack-withsupabase/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.mehc31pm32k"
+    "revision": "0.7bm9jfth7jg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
